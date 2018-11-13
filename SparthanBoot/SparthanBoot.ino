d5bb3a64ccbb53d9f28b827b8de473cd363e2c37
Davide Asnaghi <asnaghi@me.com>
2018-11-12 18:11:13 -0800

    Added serial bypass for debugging

--- SparthanBoot/SparthanBoot.ino
+++ SparthanBoot/SparthanBoot.ino
@@ -1,13 +1,14 @@
-#include <hardwareSerial.h>
+#include <HardwareSerial.h>
 
 #define LED           32U
 #define BOOT          18U
 #define RST           19U
 
-HardwareSerial STMserial(2);
+// Client ESP32
+HardwareSerial sparthan(2);           // Needed since this is an ESP32
 
 void setup() {
-  
+    
   pinMode(BOOT, OUTPUT);
   pinMode(LED, OUTPUT);
   pinMode(RST, OUTPUT);
@@ -16,17 +17,22 @@ void setup() {
   digitalWrite(RST, LOW);
   delay(100);
   pinMode(RST, INPUT);
-
-  STMserial.begin(3000000, SERIAL_8N1, 16, 17);
-
-}
-
-void loop() {
-
-  STMserial.println("TEST SERIAL");
-  digitalWrite(LED, LOW);
   delay(100);
   digitalWrite(LED, HIGH);
-  delay(100);
+  
+  Serial.begin(115200);
+  sparthan.begin(3000000, SERIAL_8N1, 16, 17);
+}
 
+void loop() {  
+  if (Serial.available())             // If data is sent to th se ESP32
+  {      
+    int c = Serial.read();
+    sparthan.write(c);                // Push it through Port 2 to STM32
+  }
+  if (sparthan.available())           // If data is received through Port 2 from STM32
+  {     
+    int c = sparthan.read();
+    Serial.write(c);                  // Show data on Serial Monitor
+  }
 }
